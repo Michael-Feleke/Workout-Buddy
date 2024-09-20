60d4095bedfde346af70c205c27c16aa7102dcfc
Michael-Feleke <felekemichael999@gmail.com>
2024-09-20 09:31:23 +0300

    Refactor workoutController to use separate functions for CRUD operations

--- src/controllers/workoutController.js
+++ src/controllers/workoutController.js
@@ -1,9 +1,8 @@
-import mongoose from "mongoose";
 import Workout from "../models/workout/model.js";
 import { isValidMongoId } from "../utils/isValidMongoId.js";
 
 const getWorkouts = async (req, res, next) => {
-  const workout = await Workout.find({});
+  const workout = await Workout.getAllWorkouts();
   res.status(200).json(workout);
 };
 
@@ -12,15 +11,16 @@ const getWorkout = async (req, res, next) => {
   if (!isValidMongoId(id))
     return res.status(404).json({ error: "No such workout" });
 
-  const workout = await Workout.findById(id);
+  const workout = await Workout.getSingleWorkout(id);
   if (!workout) return res.status(404).json({ error: "No such workout" });
   res.status(200).json(workout);
 };
 
 const createWorkout = async (req, res, next) => {
   const { title, reps, load } = req.body;
+  const newWorkout = { title, reps, load };
 
-  const workout = await Workout.create({ title, reps, load });
+  const workout = await Workout.createNewWorkout(newWorkout);
   res.status(200).json(workout);
 };
 
@@ -30,22 +30,19 @@ const deleteWorkout = async (req, res, next) => {
   if (!isValidMongoId(id))
     return res.status(404).json({ error: "No such workout" });
 
-  const workout = await Workout.findByIdAndDelete(id);
+  const workout = await Workout.deleteSingleWorkout(id);
   if (!workout) return res.status(404).json({ error: "No such workout" });
   res.status(200).json(workout);
 };
 
 const updateWorkout = async (req, res, next) => {
   const { id } = req.params;
+  const updatedWorkout = { ...req.body };
 
   if (!isValidMongoId(id))
     return res.status(404).json({ error: "No such workout" });
 
-  const workout = await Workout.findByIdAndUpdate(
-    id,
-    { ...req.body },
-    { new: true }
-  );
+  const workout = await Workout.updateWorkout(id, updatedWorkout);
   if (!workout) return res.status(404).json({ error: "No such workout" });
   res.status(200).json(workout);
 };
