b51c0a10dbca52fb5397dcee0879145113d74f4d
Michael-Feleke <felekemichael999@gmail.com>
2024-09-21 10:04:53 +0300

    Refactor database connection error handling in index.js

--- src/config/database.js
+++ src/config/database.js
@@ -1,11 +1,26 @@
 import mongoose from "mongoose";
-import { MONGO_URI } from "../utils/constants";
+import { MONGO_URI } from "../utils/constants.js";
 
 export const connectDB = async () => {
   try {
     await mongoose.connect(MONGO_URI);
+
     console.log("MongoDB connected");
+
+    mongoose.connection.on("connected", () => {
+      console.log("Mongoose connected to DB");
+    });
+
+    mongoose.connection.on("error", (err) => {
+      console.error(`Mongoose connection error: ${err.message}`);
+      throw new Error(err);
+    });
+
+    mongoose.connection.on("disconnected", () => {
+      console.log("Mongoose disconnected");
+    });
   } catch (error) {
-    console.log(`Error: ${error.message}`);
+    console.log(`Initial connection Error: ${error.message}`);
+    throw new Error("MongoDB connection failed!");
   }
 };
