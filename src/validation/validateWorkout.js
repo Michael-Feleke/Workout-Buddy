544de4224142fd560db4c0e67a14d14dd14c3e15
Michael-Feleke <felekemichael999@gmail.com>
2024-09-21 10:05:22 +0300

    Add workout validation middleware

---
+++ src/validation/validateWorkout.js
@@ -0,0 +1,36 @@
+import Joi from "joi";
+
+const workoutSchema = Joi.object({
+  title: Joi.string().trim().min(3).max(255).required().messages({
+    "string.base": "Title should be a type of text",
+    "string.empty": "Title cannot be an empty field",
+    "string.min": "Title should have a minimum length of {#limit}",
+    "string.max": "Title should have a maximum length of {#limit}",
+    "any.required": "Title is a required field",
+  }),
+  reps: Joi.number().min(1).max(255).required().messages({
+    "number.base": "Reps should be a type of number",
+    "number.empty": "Reps cannot be an empty field",
+    "number.min": "Reps should have a minimum length of {#limit}",
+    "number.max": "Reps should have a maximum length of {#limit}",
+    "any.required": "Reps is a required field",
+  }),
+  load: Joi.number().min(1).max(255).required().messages({
+    "number.base": "Load should be a type of number",
+    "number.empty": "Load cannot be an empty field",
+    "number.min": "Load should have a minimum length of {#limit}",
+    "number.max": "Load should have a maximum length of {#limit}",
+    "any.required": "Load is a required field",
+  }),
+});
+
+const validateWorkout = (req, res, next) => {
+  const { error } = workoutSchema.validate(req.body, { abortEarly: false });
+  if (error) {
+    const errorDetails = error.details.map((detail) => detail.message);
+    return res.status(400).json({ errors: errorDetails });
+  }
+  next();
+};
+
+export { validateWorkout };
