ad63116df37d3aec0fac20a588be9fdc4be3cc74
Michael-Feleke <felekemichael999@gmail.com>
2024-09-23 08:40:39 +0300

    Add authentication controller functions

---
+++ src/controllers/authController.js
@@ -0,0 +1,7 @@
+const singUpUser = async (req, res, next) => {};
+
+const logInUser = async (req, res, next) => {};
+
+const logOutUser = async (req, res, next) => {};
+
+export { singUpUser, logInUser, logOutUser };
