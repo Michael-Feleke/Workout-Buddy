b1743c52f60bfa97b445285b953cec68711e3ce8
Michael-Feleke <felekemichael999@gmail.com>
2024-09-23 11:17:45 +0300

    Refactor authRoutes.js to import authController.js with correct file extension and add signUpErrorHandler middleware to the sign-up route

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -3,12 +3,13 @@ import {
   logInUser,
   logOutUser,
   singUpUser,
-} from "../controllers/authController";
+} from "../controllers/authController.js";
 import { catchAsync } from "../utils/catchAsync.js";
+import { signUpErrorHandler } from "../middlewares/signUpErrorHandler.js";
 
 const router = express.Router();
 
-router.post("/sign-up", catchAsync(singUpUser));
+router.post("/sign-up", catchAsync(singUpUser), signUpErrorHandler);
 router.post("/log-in", catchAsync(logInUser));
 router.get("/log-out", catchAsync(logOutUser));
 
