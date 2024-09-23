2d2a94eaf16c298e44ae5fefaa5377798d0e6afd
Michael-Feleke <felekemichael999@gmail.com>
2024-09-23 11:17:03 +0300

    Refactor authController.js and implement user sign up functionality

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,4 +1,13 @@
-const singUpUser = async (req, res, next) => {};
+import User from "../models/user/model.js";
+
+const singUpUser = async (req, res, next) => {
+  const { email, password } = req.body;
+  const newUser = { email, password };
+
+  const createdUser = await User.signUpUser(newUser);
+
+  return res.status(201).send(createdUser);
+};
 
 const logInUser = async (req, res, next) => {};
 
