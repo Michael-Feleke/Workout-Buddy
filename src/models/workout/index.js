951f1b4279d550628adff57ab0a8f5b3f7b60133
Michael-Feleke <felekemichael999@gmail.com>
2024-09-25 17:23:27 +0300

    Refactor workout model to use index.js for better organization

--- src/models/workout/index.js
+++ src/models/workout/index.js
@@ -1,5 +1,8 @@
 import mongoose from "mongoose";
 import workoutSchema from "./schema.js";
+import * as Statics from "./statics.js";
+
+workoutSchema.static(Statics);
 
 const Workout = mongoose.model("Workout", workoutSchema);
 
