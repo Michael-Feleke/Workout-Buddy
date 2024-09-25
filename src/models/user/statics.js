9961997bf061d2612b5e03521ce7606637794d69
Michael-Feleke <felekemichael999@gmail.com>
2024-09-25 19:18:30 +0300

    Refactor logInUser function in user statics.js to handle authentication with bcrypt

--- src/models/user/statics.js
+++ src/models/user/statics.js
@@ -1,7 +1,19 @@
+import bcrypt from "bcrypt";
+import AppError from "../../utils/appError.js";
+
 export async function signUpUser(newUser) {
   return this.create(newUser);
 }
 
-export async function logInUser(id) {}
+export async function logInUser({ email, password }) {
+  const existedUser = await this.findOne({ email });
+
+  if (existedUser) {
+    const auth = await bcrypt.compare(password, existedUser.password);
+    if (auth) return existedUser;
+    throw new AppError("Incorrect password", 400);
+  }
+  throw new AppError("Incorrect email", 400);
+}
 
 export async function logOutUser() {}
