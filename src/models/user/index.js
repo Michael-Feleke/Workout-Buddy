40b465f751f845f154d8c7d96951accedeb1f38a
Michael-Feleke <felekemichael999@gmail.com>
2024-09-25 17:23:20 +0300

    Refactor user model to use index.js for better organization

--- src/models/user/index.js
+++ src/models/user/index.js
@@ -1,5 +1,10 @@
 import mongoose from "mongoose";
 import userSchema from "./schema.js";
+import * as Statics from "./statics.js";
+import { registerHooks } from "./hooks.js";
+
+userSchema.static(Statics);
+registerHooks(userSchema);
 
 const User = mongoose.model("User", userSchema);
 
