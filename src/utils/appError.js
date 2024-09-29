277d105d6c7d625e34953bd68585434ed7babc65
Michael-Feleke <felekemichael999@gmail.com>
2024-09-29 12:37:50 +0300

    Refactor AppError class to include errors parameter

--- src/utils/appError.js
+++ src/utils/appError.js
@@ -1,7 +1,8 @@
 class AppError extends Error {
-  constructor(message, statusCode) {
+  constructor(message, statusCode, errors = []) {
     super(message);
     this.statusCode = statusCode;
+    this.errors = errors;
     this.status = `${statusCode}`.startsWith("4") ? "fail" : "error";
     this.isOperational = true;
   }
