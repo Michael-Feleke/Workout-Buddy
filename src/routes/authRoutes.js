1f08ebe81d153c90a254709598e47f8ff2b962f0
Michael-Feleke <felekemichael999@gmail.com>
2024-09-29 09:31:49 +0300

    Refactor authRoutes.js to include user validation in sign-up and log-in routes

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -6,11 +6,12 @@ import {
 } from "../controllers/authController.js";
 import { catchAsync } from "../utils/catchAsync.js";
 import { authErrorHandler } from "../middlewares/authErrorHandler.js";
+import { validateUser } from "../validation/validateUser.js";
 
 const router = express.Router();
 
-router.post("/sign-up", catchAsync(singUpUser), authErrorHandler);
-router.post("/log-in", catchAsync(logInUser), authErrorHandler);
+router.post("/sign-up", validateUser, catchAsync(singUpUser), authErrorHandler);
+router.post("/log-in", validateUser, catchAsync(logInUser), authErrorHandler);
 router.get("/log-out", catchAsync(logOutUser));
 
 export default router;
