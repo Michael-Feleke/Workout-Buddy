3c49abe58859a4ad8637213830350af2370d7cf2
Michael-Feleke <felekemichael999@gmail.com>
2024-09-29 09:31:43 +0300

    Refactor user schema to simplify validation and remove unnecessary code

--- src/models/user/schema.js
+++ src/models/user/schema.js
@@ -1,5 +1,4 @@
 import mongoose from "mongoose";
-import isEmail from "validator/lib/isEmail.js";
 
 const Schema = mongoose.Schema;
 
@@ -7,23 +6,12 @@ const userSchema = new Schema(
   {
     email: {
       type: String,
-      required: [true, "Email is required"],
+      required: true,
       unique: true,
-      lowercase: true,
-      validate: [isEmail, "Please enter a valid email"],
     },
     password: {
       type: String,
-      required: [true, "Password is required"],
-      minlength: [8, "Password must be at least 8 characters long"],
-      validate: {
-        validator: (v) =>
-          /^(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$])(?=.*\d)[A-Za-z\d!@#$]{8,}$/.test(
-            v
-          ),
-        message: (props) =>
-          "Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character (!,@,#,$)",
-      },
+      required: true,
     },
   },
   {
