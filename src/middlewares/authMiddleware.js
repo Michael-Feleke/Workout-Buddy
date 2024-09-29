aa916c93aa21f8259abbda8fcec00bfd8603556b
Michael-Feleke <felekemichael999@gmail.com>
2024-09-29 12:37:32 +0300

    Refactor authMiddleware.js to use AppError for error handling in requireAuth middleware

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -2,30 +2,33 @@ import jwt from "jsonwebtoken";
 import { JWT_SECRET } from "../utils/constants.js";
 import User from "../models/user/index.js";
 import logger from "../config/logger.js";
+import AppError from "../utils/appError.js";
 
 export const requireAuth = (req, res, next) => {
   const token = req.cookies.jwt;
 
-  if (token) {
-    jwt.verify(token, JWT_SECRET, async (err, decodedToken) => {
-      if (err) {
-        logger.error(err.message);
-        res.status(401).send({
-          status: "error",
-          message: "Unauthorized: No token provided",
-          statusCode: 401,
-        });
-      } else {
-        const foundUser = await User.findUserById(decodedToken.id);
-        req.foundUser = foundUser;
-        next();
-      }
-    });
-  } else {
-    res.status(401).send({
-      status: "error",
-      message: "Unauthorized: No token provided",
-      statusCode: 401,
-    });
+  if (!token) {
+    return next(new AppError("Unauthorized: No token provided", 401));
   }
+
+  jwt.verify(token, JWT_SECRET, async (err, decodedToken) => {
+    if (err) {
+      logger.error(err.message);
+      return next(new AppError("Unauthorized: Invalid token", 401));
+    }
+
+    try {
+      const foundUser = await User.findById(decodedToken.id);
+
+      if (!foundUser) {
+        return next(new AppError("Unauthorized: User not found", 401));
+      }
+
+      req.foundUser = foundUser;
+      next();
+    } catch (error) {
+      logger.error(`Error retrieving user: ${error.message}`);
+      return next(new AppError("Internal server error", 500));
+    }
+  });
 };
