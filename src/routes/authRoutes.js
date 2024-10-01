1edc6a2eed0120a1b41b716de460f2a6d15d8fcb
Michael-Feleke <felekemichael999@gmail.com>
2024-10-01 14:34:58 +0300

    Refactor authRoutes.js to remove catchAsync wrapper for singUpUser, logInUser, and logOutUser functions

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -4,14 +4,13 @@ import {
   logOutUser,
   singUpUser,
 } from "../controllers/authController.js";
-import { catchAsync } from "../utils/catchAsync.js";
 import { authErrorHandler } from "../middlewares/authErrorHandler.js";
 import { validateUser } from "../validation/validateUser.js";
 
 const router = express.Router();
 
-router.post("/sign-up", validateUser, catchAsync(singUpUser), authErrorHandler);
-router.post("/log-in", validateUser, catchAsync(logInUser), authErrorHandler);
-router.get("/log-out", catchAsync(logOutUser));
+router.post("/sign-up", validateUser, singUpUser, authErrorHandler);
+router.post("/log-in", validateUser, logInUser, authErrorHandler);
+router.get("/log-out", logOutUser);
 
 export default router;
