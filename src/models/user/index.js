c120317bb5a05cb002ac08d3a033cc7b9d790dd5
Michael-Feleke <felekemichael999@gmail.com>
2024-10-01 14:34:40 +0300

    Refactor user model to include user methods

--- src/models/user/index.js
+++ src/models/user/index.js
@@ -1,9 +1,12 @@
 import mongoose from "mongoose";
 import userSchema from "./schema.js";
 import * as Statics from "./statics.js";
+import * as Methods from "./methods.js";
 import { registerHooks } from "./hooks.js";
 
 userSchema.static(Statics);
+userSchema.method(Methods);
+
 registerHooks(userSchema);
 
 const User = mongoose.model("User", userSchema);
