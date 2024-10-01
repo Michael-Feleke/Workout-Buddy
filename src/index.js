c928c909e99ad89eba6e3557cd603c3fa9bfcaa4
Michael-Feleke <felekemichael999@gmail.com>
2024-10-01 14:33:44 +0300

    Refactor index.js to include userRouter for user API routes

--- src/index.js
+++ src/index.js
@@ -1,14 +1,24 @@
+import "express-async-errors";
+
 import express from "express";
 import cookieParser from "cookie-parser";
 import helmet from "helmet";
 import morgan from "morgan";
+
 import authRouter from "./routes/authRoutes.js";
+import userRouter from "./routes/userRoutes.js";
 import workoutRouter from "./routes/workoutRoutes.js";
+
 import { connectDB } from "./config/database.js";
 import { MORGAN_FORMAT, PORT } from "./utils/constants.js";
 import { globalErrorHandler } from "./controllers/errorController.js";
 import { requireAuth } from "./middlewares/authMiddleware.js";
+import { handleUncaughtExceptions } from "./errors/uncaughtExceptions.js";
+import { handleUnhandledRejections } from "./errors/unhandledRejections.js";
 import logger from "./config/logger.js";
+
+// Setup uncaught exception handling
+handleUncaughtExceptions();
 
 //express app
 const app = express();
@@ -25,6 +35,7 @@ app.use("/auth", authRouter);
 app.use(requireAuth);
 
 //api
+app.use("/api/user", userRouter);
 app.use("/api/workout/", workoutRouter);
 
 //Error handler
@@ -34,9 +45,11 @@ app.use(globalErrorHandler);
 const startServer = async () => {
   try {
     await connectDB();
-    app.listen(PORT, () => {
+    const server = app.listen(PORT, () => {
       logger.info(`Server is running on port ${PORT}`);
     });
+
+    handleUnhandledRejections(server);
   } catch (error) {
     logger.error(`Failed to connect to the database: ${error.message}`);
     process.exit(1);
