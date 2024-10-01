64cda6a2fe39138c12037d424288a4a6e4831567
Michael-Feleke <felekemichael999@gmail.com>
2024-10-01 14:33:59 +0300

    Refactor logger configuration to use winston library and improve log file handling

--- src/config/logger.js
+++ src/config/logger.js
@@ -1,22 +1,50 @@
 import winston from "winston";
+import path from "path";
 import { ENVIRONMENT, LOG_LEVEL } from "../utils/constants.js";
+import { ensureDirectoryExists } from "../utils/ensureDirectoryExists.js";
+
+const errorLogPath = path.join("logs", "error.log");
+const combinedLogPath = path.join("logs", "combined.log");
+
+ensureDirectoryExists(errorLogPath);
+ensureDirectoryExists(combinedLogPath);
+
+const logFormat = winston.format.printf(({ timestamp, level, message }) => {
+  return ENVIRONMENT === "production"
+    ? `${timestamp} [${level}]: ${message}`
+    : `[${level}]: ${message}`;
+});
 
 const logger = winston.createLogger({
   level: LOG_LEVEL,
   format: winston.format.combine(
-    winston.format.colorize(),
-    winston.format.timestamp(),
-    winston.format.printf(({ timestamp, level, message }) => {
-      return ENVIRONMENT === "production"
-        ? `${timestamp} [${level}]: ${message}`
-        : `[${level}]: ${message}`;
-    })
+    winston.format.timestamp({ format: "YYYY-MM-DD HH:mm:ss" }),
+    winston.format.errors({ stack: true }),
+    logFormat
   ),
   transports: [
     new winston.transports.Console({
-      silent: false,
+      format: winston.format.combine(winston.format.colorize(), logFormat),
+      silent: ENVIRONMENT === "test",
     }),
   ],
 });
+
+if (ENVIRONMENT === "production") {
+  const logDirectory = path.join("logs");
+
+  logger.add(
+    new winston.transports.File({
+      filename: path.join(logDirectory, "error.log"),
+      level: "error",
+    })
+  );
+
+  logger.add(
+    new winston.transports.File({
+      filename: path.join(logDirectory, "combined.log"),
+    })
+  );
+}
 
 export default logger;
